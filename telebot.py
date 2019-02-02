d2f0096beb4bbc9ced99b817d9175f8af590d7e9
divn <Juuso.takala86@gmail.com>
2019-02-02 21:02:54 +0200

    first set of comments
    Signed-off-by: divn <Juuso.takala86@gmail.com>

--- telebot.py
+++ telebot.py
@@ -6,6 +6,7 @@ import config
 
 
 def get_updates():
+    # Get json from ur telegram bot
     url = config.URL + 'GetUpdates?timeout=100'
     content = requests.get(url)
     js = json.loads(content.content)
@@ -13,6 +14,7 @@ def get_updates():
 
 
 def get_last_chat_id_and_text(update):
+    # get last text and chatid from telegram api
     last = len(update['result']) - 1
     text = update['result'][last]['message']['text']
     chat_id = update['result'][last]['message']['chat']['id']
@@ -20,16 +22,19 @@ def get_last_chat_id_and_text(update):
 
 
 def get_last_chat_id(update):
+    # get only last chatid not text
     last = len(update['result']) - 1
     chat_id = update['result'][last]['message']['chat']['id']
     return chat_id
 
 
 def send_message(text, chat_id):
+    # Used to send message to users chatid with text
     sendmsg = 'sendMessage?chat_id={}&text={}'.format(chat_id, text)
     url = config.URL + sendmsg
     requests.post(url)
 
 
 def own_chat_id():
+    # Return static chatid set in config.py
     return config.CHATID
